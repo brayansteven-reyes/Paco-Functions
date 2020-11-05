a460f21dc164f4d813ce02acd38ced266387d591
B. Steven Reyes <brayansteven.reyes@globant.com>
2020-11-05 11:36:38 -0500

    Adding contract type and process type

---
+++ HttpTrigger5/__init__.py
@@ -0,0 +1,44 @@
+import logging
+import azure.functions as func
+import json
+from .. import connection
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    department = req.route_params.get('department')
+    year = req.params.get('year')
+    order = req.params.get('order')
+    sort = req.params.get('sort')
+    sort_values = ['count','total']
+    order_values = ['asc','desc']
+
+    if not year:
+        year = '1990'
+    if sort: 
+        if sort.lower() not in sort_values:
+            sort = 'count'
+    else:
+        sort = 'count'
+    if order: 
+        if order.lower() not in order_values:
+            order = 'desc'
+    else:
+        order = 'desc'
+    
+    query_result =  connection.query_db(f"""Select TIPO_PROCESO as contractor,
+                            count(*) as 'count',
+                            CAST(sum(VALOR_TOTAL_CONTRATO) AS UNSIGNED) as total
+                            from contratos 
+                            where DEPARTAMENTO = '{department.upper()}'
+                            and year(FECHA_FIRMA_CONTRATO)>'{year}'
+                            group by TIPO_PROCESO 
+                            order by {sort.lower()} {order};""")
+        
+    result_str = json.dumps(query_result)
+    func.HttpResponse.mimetype = 'application/json'
+    func.HttpResponse.charset = 'utf-8'
+    return func.HttpResponse(
+            result_str,
+            status_code=200
+    )
