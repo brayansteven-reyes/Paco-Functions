118a3cba083c34810b8ad4012e3efa60ec3769a7
B. Steven Reyes <brayansteven.reyes@globant.com>
2020-11-26 16:08:09 -0500

    Adding siri sanctions

--- HttpTrigger5/__init__.py
+++ HttpTrigger5/__init__.py
@@ -6,9 +6,23 @@ from .. import query_utils
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    query = f"""select departamento  as department
-                from contratos
-                group by 1;"""
+    department = req.route_params.get('department')
+    sort_values = ['DEPARMENT', 'COUNT']
+
+    conditions = query_utils.getCommonCondtions(req)
+    order_by = query_utils.getCommontOrderBy(req, sort_values, sort_values[1])
+    limits = query_utils.getCommontLimit(req)
+
+    if department:
+        conditions += f" AND DEPARTAMENTO = '{department.upper()}'"
+    query = f"""Select DEPARTAMENTO as department,
+                CAST(sanciones AS UNSIGNED) as count
+                from siri_sanciones_departamento 
+                where 1=1
+                {conditions}
+                {order_by} 
+                {limits};"""
+                
     query_result = connection.query_db(query)
     result_str = json.dumps(query_result)
 
